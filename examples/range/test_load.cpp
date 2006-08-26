a322d21e931a41ef2896785184fea5696b04e6b5
Paul Grenyer <paul.grenyer@gmail.com>
2006-08-26 12:13:06 +0000

    Added negative ranges to test_load.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/trunk@129 48d1c4d4-5707-0410-b125-91f9528d8bec

--- examples/range/test_load.cpp
+++ examples/range/test_load.cpp
@@ -6,11 +6,17 @@ namespace Range
 {
 	namespace
 	{
-		const int inc[] = {0,1,2,3,4,5,6,7,8,9,10};	
+		const int inc[] = {0,1,2,3,4,5,6,7,8,9,10};
 		const int dec[] = {10,9,8,7,6,5,4,3,2,1,0};
 
 		const VectorOfInt goingUp( inc, inc + sizeof( inc ) / sizeof( VectorOfInt::value_type ) );
 		const VectorOfInt goingDown( dec, dec + sizeof( dec ) / sizeof( VectorOfInt::value_type ) );
+
+		const int negativeInc[] = {-5,-4,-3,-2,-1,0,1,2,3,4,5};
+		const int negativeDec[] = {5,4,3,2,1,0,-1,-2,-3,-4,-5};
+
+		const VectorOfInt negativeGoingUp( negativeInc, negativeInc + sizeof( negativeInc ) / sizeof( VectorOfInt::value_type ) );
+		const VectorOfInt negativeGoingDown( negativeDec, negativeDec + sizeof( negativeDec ) / sizeof( VectorOfInt::value_type ) );
 			
 	}	
 
@@ -21,5 +27,11 @@ namespace Range
 
 		vecToTest = load( 10, 0 );
 		IS_EQUAL( goingDown, vecToTest );
+
+		vecToTest = load( -5, 5 );
+		IS_EQUAL( negativeGoingUp, vecToTest );
+
+		vecToTest = load( 5, -5 );
+		IS_EQUAL( negativeGoingDown, vecToTest );
 	}
 }
