a43d7143032159e488b5defdad89faf8a1fc3b7a
Paul Grenyer <paul.grenyer@gmail.com>
2006-10-03 14:26:35 +0000

    Added list example project.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/trunk@145 48d1c4d4-5707-0410-b125-91f9528d8bec

---
+++ examples/List/TestClient/main.cpp
@@ -0,0 +1,24 @@
+
+#include <aeryn/test_runner.hpp>
+#include <aeryn/test_name_not_found.hpp>
+#include <aeryn/command_line_parser.hpp>
+
+int main( int, char *argv[] )
+{
+	using namespace Aeryn;
+	int result = -1;
+
+	try
+	{
+		TestRunner testRunner;
+
+		CommandLineParser commandLineParser( argv );
+		result = testRunner.Run( commandLineParser );
+	}
+	catch( const Exception& e )
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return result;
+}
