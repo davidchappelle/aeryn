df2e01637503aca582f0d072ad37c18ea3e56c59
Paul Grenyer <paul.grenyer@gmail.com>
2007-11-23 23:55:24 +0000

    Removed unnecessary using directives.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/branches/prototypes@203 48d1c4d4-5707-0410-b125-91f9528d8bec

--- AerynGUI/Marauder.Aeryn.Forms/MainForm.cs
+++ AerynGUI/Marauder.Aeryn.Forms/MainForm.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Text;
+using System.Text;*/
 using System.Windows.Forms;
 
 namespace Marauder.Aeryn.Forms
