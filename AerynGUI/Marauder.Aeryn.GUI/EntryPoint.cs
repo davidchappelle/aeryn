31f0b46ca26c05295df957502ba0db9e969001b5
Paul Grenyer <paul.grenyer@gmail.com>
2007-11-23 23:32:44 +0000

    Added AerynGUI prototype.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/branches/prototypes@201 48d1c4d4-5707-0410-b125-91f9528d8bec

---
+++ AerynGUI/Marauder.Aeryn.GUI/EntryPoint.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+using Marauder.Aeryn.Forms;
+
+namespace Marauder.Aeryn.GUI
+{
+    static class EntryPoint
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
