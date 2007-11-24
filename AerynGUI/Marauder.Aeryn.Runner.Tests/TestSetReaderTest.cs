20def1889ae2164610f77dd7a05c15e4fc3d5ebe
Paul Grenyer <paul.grenyer@gmail.com>
2007-11-24 06:51:18 +0000

    Added initial test.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/branches/prototypes@207 48d1c4d4-5707-0410-b125-91f9528d8bec

--- AerynGUI/Marauder.Aeryn.Runner.Tests/TestSetReaderTest.cs
+++ AerynGUI/Marauder.Aeryn.Runner.Tests/TestSetReaderTest.cs
@@ -1,10 +1,16 @@
 
 using NUnit.Framework;
+using Marauder.Aeryn.Runner;
 
 namespace Marauder.Aeryn.Runner.Tests
 {
     [TestFixture]
     public class TestSetReaderTest
     {
+        [Test]
+        public void TestSetAndTestsTest()
+        {
+            TestSetReader tsr = new TestSetReader();
+        }
     }
 }
--- AerynGUI/Marauder.Aeryn.Runner/TestSetReader.cs
+++ AerynGUI/Marauder.Aeryn.Runner/TestSetReader.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Marauder.Aeryn.Runner
 {
-    class TestSetReader
+    public class TestSetReader
     {
     }
 }
