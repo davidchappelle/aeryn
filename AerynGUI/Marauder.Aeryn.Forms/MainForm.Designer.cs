d8ab304ae780a8efe58e6680b4d453123bf727bd
Paul Grenyer <paul.grenyer@gmail.com>
2007-11-24 00:25:10 +0000

    Added tree view for test sets.
    git-svn-id: http://paulgrenyer.net/svn/aeryn/branches/prototypes@205 48d1c4d4-5707-0410-b125-91f9528d8bec

--- AerynGUI/Marauder.Aeryn.Forms/MainForm.Designer.cs
+++ AerynGUI/Marauder.Aeryn.Forms/MainForm.Designer.cs
@@ -28,11 +28,82 @@ namespace Marauder.Aeryn.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testSets = new System.Windows.Forms.TreeView();
+            this.testSetGroupBox = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(679, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(40, 22);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // testSets
+            // 
+            this.testSets.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.testSets.Location = new System.Drawing.Point(29, 61);
+            this.testSets.Name = "testSets";
+            this.testSets.Size = new System.Drawing.Size(275, 399);
+            this.testSets.TabIndex = 1;
+            // 
+            // testSetGroupBox
+            // 
+            this.testSetGroupBox.Location = new System.Drawing.Point(13, 39);
+            this.testSetGroupBox.Name = "testSetGroupBox";
+            this.testSetGroupBox.Size = new System.Drawing.Size(309, 441);
+            this.testSetGroupBox.TabIndex = 2;
+            this.testSetGroupBox.TabStop = false;
+            this.testSetGroupBox.Text = "Test Sets";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 502);
+            this.Controls.Add(this.testSets);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.testSetGroupBox);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TreeView testSets;
+        private System.Windows.Forms.GroupBox testSetGroupBox;
     }
 }
--- AerynGUI/Marauder.Aeryn.Forms/MainForm.cs
+++ AerynGUI/Marauder.Aeryn.Forms/MainForm.cs
@@ -13,6 +13,29 @@ namespace Marauder.Aeryn.Forms
         public MainForm()
         {
             InitializeComponent();
+
+            TreeNode testSet1 = this.testSets.Nodes.Add("Test Set 1");
+            testSet1.Nodes.Add("Test 1");
+            testSet1.Nodes.Add("Test 2");
+
+
+            TreeNode testSet2 = this.testSets.Nodes.Add("Test Set 2");
+            testSet2.Nodes.Add("Test 3");
+            testSet2.Nodes.Add("Test 4");
+
+            testSets.ExpandAll();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using( OpenFileDialog ofd = new OpenFileDialog() )
+            {
+                DialogResult result = ofd.ShowDialog();
+                if (DialogResult.OK == result)
+                {
+                    this.Text = ofd.FileName;
+                }
+            }
         }
     }
 }
